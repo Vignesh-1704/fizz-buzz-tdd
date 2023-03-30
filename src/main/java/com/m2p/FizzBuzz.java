8b4acdffdf0b76b90c74a81ae4990a9b3657d14b
Vignesh-1704 <vigneshmanikavasagam17@gmail.com>
2023-03-30 09:50:57 +0530

    Requirement 2 Add. Included the Logic And Test Cases for Requirement 2

--- src/main/java/com/m2p/FizzBuzz.java
+++ src/main/java/com/m2p/FizzBuzz.java
@@ -4,16 +4,19 @@ public class FizzBuzz {
     public String getFizzBuzz(int inputNumber) {
         if(inputNumber < 1 || inputNumber > 100)
             throw new IllegalArgumentException();
-        else if(inputNumber % 3 == 0)
+        else if(inputNumber % 3 == 0 && inputNumber % 5 == 0)
+        {
+            return "FizzBuzz";
+        }
+        else if(inputNumber % 3 == 0 || String.valueOf(inputNumber).contains("3"))
         {
             return "Fizz";
         }
-        else if(inputNumber % 5 == 0)
+        else if(inputNumber % 5 == 0 || String.valueOf(inputNumber).contains("5"))
         {
             return "Buzz";
         }
-        else
-        {
+        else {
             return String.valueOf(inputNumber);
         }
     }
--- src/test/java/com/m2p/FizzBuzzTest.java
+++ src/test/java/com/m2p/FizzBuzzTest.java
@@ -1,5 +1,6 @@
 package com.m2p;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,9 +9,11 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class FizzBuzzTest {
     FizzBuzz fizzBuzzNumber = new FizzBuzz();
 
+
+    @Nested
+    class Requirement1{
     @Test
-    void toReturnFizzIfSixIsGivenAsInput()
-    {
+    void toReturnFizzIfSixIsGivenAsInput() {
         //Arrange
         String expectedString = "Fizz";
 
@@ -18,12 +21,11 @@ public class FizzBuzzTest {
         String actualString = fizzBuzzNumber.getFizzBuzz(6);
 
         //Assert
-        assertEquals(expectedString,actualString);
+        assertEquals(expectedString, actualString);
     }
 
     @Test
-    void toReturnBuzzIfTenIsGivenAsInput()
-    {
+    void toReturnBuzzIfTenIsGivenAsInput() {
         //Arrange
         String expectedString = "Buzz";
 
@@ -31,12 +33,11 @@ public class FizzBuzzTest {
         String actualString = fizzBuzzNumber.getFizzBuzz(10);
 
         //Assert
-        assertEquals(expectedString,actualString);
+        assertEquals(expectedString, actualString);
     }
 
     @Test
-    void toReturnTheSameNumberIfItIsNotDivisibleByBothThreeAndFive()
-    {
+    void toReturnTheSameNumberIfItIsNotDivisibleByBothThreeAndFive() {
         //Arrange
         String expectedResult = "11";
 
@@ -44,12 +45,53 @@ public class FizzBuzzTest {
         String actualResult = fizzBuzzNumber.getFizzBuzz(11);
 
         //Assert
-        assertEquals(expectedResult,actualResult);
+        assertEquals(expectedResult, actualResult);
     }
 
     @Test
-    void toReturnExceptionIfInputNumberIsNegativeOrGreaterThanHundred()
-    {
-        assertThrows(IllegalArgumentException.class,()-> fizzBuzzNumber.getFizzBuzz(0));
+    void toReturnExceptionIfInputNumberIsNegativeOrGreaterThanHundred() {
+        assertThrows(IllegalArgumentException.class, () -> fizzBuzzNumber.getFizzBuzz(0));
     }
+
+}
+
+    @Nested
+    class Requirement2 {
+        @Test
+        void toReturnFizzAsThirtyIsDivisibleByThreeAndContainsThree() {
+            //Arrange
+            String expectedResult = "Fizz";
+
+            //Act
+            String actualResult = fizzBuzzNumber.getFizzBuzz(33);
+
+            //Assert
+            assertEquals(expectedResult, actualResult);
+        }
+
+        @Test
+        void toReturnBuzzAsFiftyIsDivisibleByFiveAndContainsFive() {
+            //Arrange
+            String expectedResult = "Buzz";
+
+            //Act
+            String actualResult = fizzBuzzNumber.getFizzBuzz(50);
+
+            //Assert
+            assertEquals(expectedResult, actualResult);
+        }
+
+        @Test
+        void toReturnFizzBuzzIfGivenInputIsFifteenWhichIsDivisibleByBothThreeAndFive() {
+            //Arrange
+            String expectedResult = "FizzBuzz";
+
+            //Act
+            String actualResult = fizzBuzzNumber.getFizzBuzz(15);
+
+            //Assert
+            assertEquals(expectedResult, actualResult);
+        }
+    }
+
 }
