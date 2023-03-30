075e971f3cd585c027b72dfaaf596be85a6aa6ef
Vignesh-1704 <vigneshmanikavasagam17@gmail.com>
2023-03-30 09:37:11 +0530

    Requirement 1 Add. Included the Logic and Test Case To Return Fizz ,Buzz , Actual Number or Necessary Conditions

--- src/main/java/com/m2p/FizzBuzz.java
+++ src/main/java/com/m2p/FizzBuzz.java
@@ -1,4 +1,20 @@
 package com.m2p;
 
 public class FizzBuzz {
+    public String getFizzBuzz(int inputNumber) {
+        if(inputNumber < 1 || inputNumber > 100)
+            throw new IllegalArgumentException();
+        else if(inputNumber % 3 == 0)
+        {
+            return "Fizz";
+        }
+        else if(inputNumber % 5 == 0)
+        {
+            return "Buzz";
+        }
+        else
+        {
+            return String.valueOf(inputNumber);
+        }
+    }
 }
--- src/test/java/com/m2p/FizzBuzzTest.java
+++ src/test/java/com/m2p/FizzBuzzTest.java
@@ -1,5 +1,55 @@
 package com.m2p;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class FizzBuzzTest {
     FizzBuzz fizzBuzzNumber = new FizzBuzz();
+
+    @Test
+    void toReturnFizzIfSixIsGivenAsInput()
+    {
+        //Arrange
+        String expectedString = "Fizz";
+
+        //Act
+        String actualString = fizzBuzzNumber.getFizzBuzz(6);
+
+        //Assert
+        assertEquals(expectedString,actualString);
+    }
+
+    @Test
+    void toReturnBuzzIfTenIsGivenAsInput()
+    {
+        //Arrange
+        String expectedString = "Buzz";
+
+        //Act
+        String actualString = fizzBuzzNumber.getFizzBuzz(10);
+
+        //Assert
+        assertEquals(expectedString,actualString);
+    }
+
+    @Test
+    void toReturnTheSameNumberIfItIsNotDivisibleByBothThreeAndFive()
+    {
+        //Arrange
+        String expectedResult = "11";
+
+        //Act
+        String actualResult = fizzBuzzNumber.getFizzBuzz(11);
+
+        //Assert
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    void toReturnExceptionIfInputNumberIsNegativeOrGreaterThanHundred()
+    {
+        assertThrows(IllegalArgumentException.class,()-> fizzBuzzNumber.getFizzBuzz(0));
+    }
 }
